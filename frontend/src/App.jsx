7d8e8ddaf7aa3e425e738504d3e994a6ebba0c9c
Tanish Negi <165296839+Tanishnegi012@users.noreply.github.com>
2025-05-29 18:20:11 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -36,7 +36,13 @@ const App = () => {
   return (
     <div data-theme={theme}>
       <Navbar />
-
+<Routes>
+        <Route path="/" element={authUser ? <HomePage /> : <Navigate to="/login" />} />
+        <Route path="/signup" element={!authUser ? <SignUpPage /> : <Navigate to="/" />} />
+        <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route path="/profile" element={authUser ? <ProfilePage /> : <Navigate to="/login" />} />
+      </Routes>
    
       <Toaster />
     </div>
