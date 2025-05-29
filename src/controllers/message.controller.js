02e4df5c59958c46a0d23e8961254c29b2a22ab5
Deepak45-sys <deepakjoshi80572@gmail.com>
2025-05-29 17:46:49 +0530

    Update message.controller.js
    there some error

--- src/controllers/message.controller.js
+++ src/controllers/message.controller.js
@@ -2,10 +2,7 @@
 
     await newMessage.save();
 
-    const receiverSocketId = getReceiverSocketId(receiverId);
-    if (receiverSocketId) {
-      io.to(receiverSocketId).emit("newMessage", newMessage);
-    }
+ 
 
     res.status(201).json(newMessage);
   } catch (error) {
